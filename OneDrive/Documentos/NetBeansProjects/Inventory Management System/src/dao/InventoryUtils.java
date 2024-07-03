a4506a2aa410f6e74faa062530a60eb66270930a
casma <casma@Alan>
2024-07-03 15:39:21 -0600

    Carga inicial

---
+++ OneDrive/Documentos/NetBeansProjects/Inventory Management System/src/dao/InventoryUtils.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author casma
+ */
+public class InventoryUtils {
+    public static String billPath = "C:\\Users\\casma\\OneDrive\\Documentos\\NetBeansProjects\\Inventory Management System\\";
+}
